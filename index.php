52900455ea60bd32fb0a20ba3356dca14a35a3c2
ryandnieves <ryandeannieves@gmail.com>
2017-03-28 15:51:03 -0400

    code to study

--- index.php
+++ index.php
@@ -1,4 +1,47 @@
 <?php
-  echo 'this is a test';
-
+$num = 5;
+$message = "This is a message";
+$name = "Ryan";
+/*
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+} 
+*/
+$fname = "Ryan";
+$lname = "Nieves";
+	
+if ($fname == "Ryan" && $fname == "Nieves"){
+	echo "Result 1";
+}
+else {
+	echo "Result 2";
+}
+/*
+while ($string != "Two"){
+	echo $string;
+	$string = "Two";
+}
+*/
+//echo $message;
+//echo $_GET['fname'];
+//echo " ";
+//echo $_GET['lname'];
+/*
+if ($_GET['num'] > 2){
+	echo "Greater than 2!";
+	
+	if ($name == "Mike") {
+		echo " Hi Mike";
+	}
+	
+}
+else if ($_GET['num'] == 2){
+	echo "Equal to 2!";
+}
+else {
+	echo "Less than 2!";
+}
+*/
+//$lname = "Lee";
+//$fname = "Mike $lname ";
 ?>
